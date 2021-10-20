ad6fa3f6362785d9e733d3e2ea7262ee16c0f2f8
Sander Janssens <sander.janssens@outlook.be>
2021-10-20 14:55:56 +0200

    Initial commit

---
+++ src/main/java/com/switchfullygroupproject/marsdigibooky/MarsDigibookyApplication.java
@@ -0,0 +1,13 @@
+package com.switchfullygroupproject.marsdigibooky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsDigibookyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsDigibookyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/switchfullygroupproject/marsdigibooky/MarsDigibookyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.switchfullygroupproject.marsdigibooky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsDigibookyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
