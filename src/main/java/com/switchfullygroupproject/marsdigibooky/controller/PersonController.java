3c6794b09752797b877584cb0d6df37876724ca7
Ralph <ralphput@hotmail.com>
2021-10-21 13:34:53 +0200

    n toegevoegd

--- src/main/java/com/switchfullygroupproject/marsdigibooky/controller/PersonController.java
+++ src/main/java/com/switchfullygroupproject/marsdigibooky/controller/PersonController.java
@@ -1,5 +1,15 @@
 package com.switchfullygroupproject.marsdigibooky.controller;
 
+import com.switchfullygroupproject.marsdigibooky.domain.person.PersonDTO;
+import com.switchfullygroupproject.marsdigibooky.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.List;
+
 public class PersonController {
 
     private final PersonService personService;
@@ -9,7 +19,7 @@ public class PersonController {
         this.personService = personService;
     }
 
-    @GetMapping(path = "{/uuid}", produces = "applicatio/json")
+    @GetMapping(path = "{/uuid}", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public PersonDTO findById(@PathVariable String uuid){
         return personService.findById(uuid);
