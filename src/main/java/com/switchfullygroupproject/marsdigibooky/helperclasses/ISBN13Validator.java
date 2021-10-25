4228709f795864b3fa96df1f8983c456b8cd27fd
myrtheververs <myrthe.ververs@hotmail.com>
2021-10-25 14:58:16 +0200

    isbn validator

---
+++ src/main/java/com/switchfullygroupproject/marsdigibooky/helperclasses/ISBN13Validator.java
@@ -0,0 +1,15 @@
+package com.switchfullygroupproject.marsdigibooky.helperclasses;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ISBN13Validator {
+    private static final String regex =
+            "^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?[0-9]+[- ]?[0-9]+[- ]?[0-9]$";
+
+    public static boolean isValidISBN(String isbn) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(isbn);
+        return matcher.matches();
+    }
+}
